b0d0717aa31bb0668de505184f23861cd4e0a387
seeratbhatia <b.seerat@gmail.com>
2022-04-12 01:09:13 +0530

    block-4

--- block-BNaabo/server.js
+++ block-BNaabo/server.js
@@ -2,13 +2,20 @@ var express = require('express');
 
 var app = express();
 
-app.use((req, res, next) => {
-    console.log(req.method, req.url);
-    next()
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', (req, res) => {
-    res.send('Welcome to Express');
+    res.sendFile(__dirname + "/index.html")
+});
+
+app.post('/json', (req, res) => {
+    console.log(req.body);
+});
+
+app.post('/contact', (req, res) => {
+    console.log(req.body);
 })
 
 app.listen(4000, () => {
