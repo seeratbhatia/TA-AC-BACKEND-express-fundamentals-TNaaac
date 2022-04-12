b2e83787760df740df4075c89f3cc97ffa336867
seeratbhatia <b.seerat@gmail.com>
2022-04-12 16:29:45 +0530

    block-2

--- block-BNaabu/sample/server.js
+++ block-BNaabu/sample/server.js
@@ -1,30 +1,30 @@
 var express = require('express');
-var cookieParser = require('cookie-Parser');
 var logger = require('morgan');
 
 var app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + "/public"));
 app.use(logger('dev'));
 
+app.use('/admin', (req, res, next) => {
+    next('Unauthorized to access');
+})
+
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile("Welcome");
 });
 
-app.get('/new', (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+app.get('/about', (req, res) => {
+    res.send('About Page');
+})
+
+app.use(( req, res, next) => {
+    res.send('Page not Found')
 });
 
-app.post('/new', (req, res) => {
-    res.json(req.body);
-});
-
-app.get('/user/:username', (req, res) => {
-    var username = req.params.username;
-    res.send(username)
-});
+app.use((err, req, res, next) => {
+    res.status(5000).send(err);
+})
 
 app.listen(4000, () => {
     console.log('server is listening on port 4k')
