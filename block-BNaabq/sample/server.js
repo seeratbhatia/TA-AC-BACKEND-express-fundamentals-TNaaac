bcbdafcc02b16213df0f6a84f5b604690ff0e350
seeratbhatia <b.seerat@gmail.com>
2022-04-12 14:06:55 +0530

    block-6

--- block-BNaabq/sample/server.js
+++ block-BNaabq/sample/server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var cookieParser = require('cookie-Parser');
+var logger = require('morgan');
 
 var app = express();
 
@@ -6,17 +8,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 
+app.use(cookieParser());
+
+app.use(logger('tiny'));
+
+app.use((req, res, next) => {
+    console.log(req.cookie);
+    next();
+});
+
+app.use("/about", (req, res, next) => {
+    res.cookie('username', 'xyz');
+    res.end('About Page');
+})
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html")
 });
 
-app.post('/json', (req, res) => {
-    console.log(req.body);
-});
 
-app.post('/contact', (req, res) => {
-    console.log(req.body);
-})
 
 app.listen(4000, () => {
     console.log('server is listening on port 4k')
