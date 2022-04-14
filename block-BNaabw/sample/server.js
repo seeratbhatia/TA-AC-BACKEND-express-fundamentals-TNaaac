db04e69ba8f01233025f8a3bd236f8092e99eb4f
seeratbhatia <b.seerat@gmail.com>
2022-04-14 18:10:15 +0530

    assignment-2

--- block-BNaabw/sample/server.js
+++ block-BNaabw/sample/server.js
@@ -1,44 +1,33 @@
+// require
+
 var express = require('express');
-
-// reqire 
-
-var app = express();
 var logger = require('morgan');
 var cookieParser = require('cookieParser');
+
+// instantiate the app
+
+var app = express();
 
 // middleware
 
 app.use(express.json());
 app.use(express.urlencoded({ extented: false }));
-
-app.use(logger('dev'));
+app.use(express.static(__dirname + "/public"));
+app.use(logger("dev"));
 app.use(cookieParser());
 
-app.use((req, res, next) => {
-    res.cookie("username", "xyz");
-    next();
-})
 
 // routes
 app.get('/', (req, res) => {
-    res.send('<h2>Welcome to express</h2>')
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/about', (req, res) => {
-    res.send('My name is qwerty');
+app.get('/projects', (req, res) => {
+    res.sendFile(__dirname + "/projects.html");
 });
 
-app.post('/form', (req, res) => {
-    res.json(req.body);
-});
-
-app.post('/json', (req, res) => {
-    res.json(req.body);
-});
-
-app.get('/user/:username', (req, res) => {
-    var username = req.params.username;
-    res.send('<h2>${username}</h2>')
+app.get('/user', (req, res) => {
+    res.send('Users Page');
 });
 
 //404 middleware
@@ -46,13 +35,13 @@ app.use((req, res, next) => {
     res.send('Page Not Found');
 });
 
-//custom middleware
+//custom client/server error middleware
 
 app.use((err, req, res, next) => {
     res.send('err');
 });
 
 
-app.listen(3000, () => {
-    console.log('server is listening on port 3k')
+app.listen(4000, () => {
+    console.log('server is listening on port 4k')
 })
